432995b841ab77b3c1181cadc849e0120036493f
Madusanka <42802790+xzunk@users.noreply.github.com>
2025-02-02 15:07:34 +0530

    Update StockCalculator.tsx

--- src/components/StockCalculator.tsx
+++ src/components/StockCalculator.tsx
@@ -96,7 +96,7 @@ const StockCalculator = () => {
               { label: 'Earnings Per Share (LKR)', name: 'eps' },
               { label: 'Total Equity (LKR Mn)', name: 'totalEquity' },
               { label: 'Total Revenue (LKR Mn)', name: 'totalRevenue' },
-              { label: 'Operating Cash Flow (LKR Mn)', name: 'operatingCashFlow' },
+           
               { label: 'Outstanding Shares (Mn)', name: 'outstandingShares' },
               { label: 'Net Income (LKR Mn)', name: 'netIncome' },
               { label: 'Total Assets (LKR Mn)', name: 'totalAssets' },
