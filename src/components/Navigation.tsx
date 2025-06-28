4085c23bfa9db41b177491326838f944ce217d76
Madusanka <42802790+xzunk@users.noreply.github.com>
2025-06-28 18:29:34 +0530

    Update Navigation.tsx
    Signed-off-by: Madusanka <42802790+xzunk@users.noreply.github.com>

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,7 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="font-bold text-xl text-white">
-            Peeper
+            Market
           </Link>
           
           <div className="hidden md:flex items-center space-x-8">
